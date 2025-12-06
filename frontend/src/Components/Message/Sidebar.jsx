913c739dae60749956fb7586d774f5aae9496ca0
Kachen Chaithet <chaytheskhchenthr@gmail.com>
2025-12-06 20:11:37 +0700

    fix: remove extraneous character in Sidebar component

--- frontend/src/Components/Message/Sidebar.jsx
+++ frontend/src/Components/Message/Sidebar.jsx
@@ -53,7 +53,7 @@ const Sidebar = ({ friends }) => {
                                 <img src={person.avatarUrl} alt="" className="w-10 h-10 rounded-full" />
                                 <span className={`w-3 h-3 ${isOnline ? 'bg-green-600' : 'bg-gray-600'}  border-2 border-white rounded-full absolute bottom-0 right-0`}></span>
 
-                            </div>?
+                            </div>
 
                             <div className="">
                                 <h1 className={`text-xl ${activeFriend && 'text-blue-500'} font-sans`}>{person.username}</h1>
